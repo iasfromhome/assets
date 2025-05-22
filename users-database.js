d50746c69c02b17deed70b7dc312ce62abb8eb7e
IAS From Home <primedrectorbdrs@gmail.com>
2025-05-22 14:45:31 +0530

    Update users-database.js
    Updated

--- users-database.js
+++ users-database.js
@@ -1,4 +1,50 @@
-// export this to the website
+/* THE DATABASE ARRAY:
+==============================*/
 const usersDatabase = [
-  // objects
-  ];
+//00001:_____
+  {
+    id:"001",
+    plan:"Passive!",
+    limit:30
+  },
+//00002:_____
+  {
+    id:"002",
+    plan:"Active!",
+    limit:60
+  },
+//00003:_____
+  {
+    id:"003",
+    plan:"Passive!",
+    limit:90
+  }
+];
+
+
+/* FIND THE USER INDEX:
+===============================*/
+var userIndex = userDatabase.findIndex(function(obj) {
+  return obj.id === userInfo.userID;
+});
+
+
+/* UPDATE USER INFO INSTANTLY:
+===============================*/
+(function userplanUpdate() {
+  var user = userDatabase[userIndex];
+  if (user && user.id===userInfo.userID) {
+    userInfo.mentorship = user.plan;
+    userInfo.maxLimit = user.limit;
+    alert(JSON.stringify(userInfo));
+  } else {
+    console.log("Not Registered!")
+  }
+})();
+//-- (end) ----->
+
+
+
+
+// The End___
+
