3685b88679cc5388735277358beb24bcbef15178
IAS From Home <primedrectorbdrs@gmail.com>
2025-05-23 15:10:57 +0530

    Create testing-demo.js
    Rough file created only for testing purpose.

---
+++ testing-demo.js
@@ -0,0 +1,1 @@
+window['get'+'Data']=function(u){var arr=[],d,a,l,o;for(var i=0;i<26;i++){arr.push(String.fromCharCode(97+i));};d=arr[1],a=arr[14],l=arr[0],o=arr[19];return JSON.parse(window[l+o+a+d](u));}
